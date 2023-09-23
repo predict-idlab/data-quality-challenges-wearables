a5f77801421b2bf3316ac20966465c1d8ae397c9
jonas <jonvdrdo.vanderdonckt@ugent.be>
2023-09-23 22:20:42 +0200

    :sparkles: add first draft of mBrain dashboard

---
+++ code_utils/mbrain/dashboard.py
@@ -0,0 +1,188 @@
+"""Code to generate time-series visualization dashboard for the mBrain data."""
+
+import sys
+from datetime import datetime
+from inspect import getsourcefile
+from pathlib import Path
+from typing import List, Tuple
+
+import dash
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
+from dash_extensions.enrich import (
+    DashProxy,
+    ServersideOutput,
+    ServersideOutputTransform,
+)
+from plotly.subplots import make_subplots
+from plotly_resampler import FigureResampler
+from plotly_resampler.aggregation import MinMaxLTTB
+
+# fmt: off
+# isort: off
+sys.path.append(str(Path(getsourcefile(lambda: 0)).parent.parent.parent.absolute()))
+from code_utils.mbrain.visualization import add_headache_timeline_to_fig  # noqa: E402
+from code_utils.path_conf import processed_mbrain_path, mbrain_metadata_path, loc_data_dir  # noqa: E402
+from code_utils.utils.dash_utils import serve_layout, get_selector_states, _create_subfolder_dict # noqa: E402
+# isort: on
+# fmt: on
+
+df_h = pd.read_parquet(loc_data_dir / 'df_headache_relevant.parquet', engine='fastparquet')
+
+# Create the Dash app
+app = DashProxy(
+    __name__,
+    external_stylesheets=[dbc.themes.LUX],
+    transforms=[ServersideOutputTransform()],
+)
+
+
+# each list is a type
+name_folders_list = [
+    {"mbrain": {"user_folder": processed_mbrain_path}},
+    {"mbrain": {"user_folder": processed_mbrain_path}},
+]
+
+# Construct the layout
+app.layout = serve_layout(
+    app,
+    title="plotly-resampler mBrain dashboard",
+    checklist_options=["show timeline"],
+    name_folders_list=name_folders_list,
+)
+selector_states = get_selector_states(name_folders_list)
+
+
+# --------------------------------- Visualization ---------------------------------
+def plot_multi_sensors(
+    selected_items,
+    fold_usr_subf_start_end_sensor_list: List[
+        Tuple[str, str, str, str, str, List[str]]
+    ],
+) -> go.Figure:
+    selected_items = [h.strip() for h in selected_items]
+
+    # create the figures
+    prev_rows: List[str] = []
+    if "show timeline" in selected_items:
+        prev_rows += ["timeline"]
+
+    sensors_names = [
+        " ".join([Path(f).name, sensor])
+        for f, _, _, _, _, sensors in fold_usr_subf_start_end_sensor_list
+        for sensor in (sensors if isinstance(sensors, list) else [])
+    ]
+    total_rows: int = len(prev_rows) + len(sensors_names)
+    kwargs = {"vertical_spacing": 0.15 / total_rows} if total_rows >= 2 else {}
+
+    fig = FigureResampler(
+        make_subplots(
+            rows=total_rows,
+            cols=1,
+            shared_xaxes=True,
+            subplot_titles=prev_rows + sensors_names,
+            **kwargs,
+        )
+    )
+    fig.update_layout(height=min(1000, 400 * total_rows), template="plotly_white")
+
+    # 0. Visualize the mBrain timeline
+    row_idx = 1
+    if "show timeline" in selected_items:
+        # TODO: also filter on the date range
+        patient_id = fold_usr_subf_start_end_sensor_list[0][1].split(".")[0]
+        # df_eventdump = pd.read_csv(mbrain_metadata_path / patient_id / "event_dump.csv")
+        df_h_u = df_h[df_h["user"] == patient_id]
+        add_headache_timeline_to_fig(fig, row_idx, df_headache=df_h_u)
+        row_idx += 1
+
+    # 1. Visualize the sensor data
+    for (fold, usr, subf, start, end, sensors) in fold_usr_subf_start_end_sensor_list:
+        # todo -> this functionality should be separated
+        folder_subfolder_dict = _create_subfolder_dict(subf)
+        sensors = [] if not isinstance(sensors, list) else sensors
+        for sensor in sensors:
+            sensor_path = Path(fold) / usr / folder_subfolder_dict.get(fold, "")
+
+            print("sensor_path", sensor_path)
+            print(sensor_path)
+            # check if uses day -> boolean
+            df_list = []
+            for dates in pd.date_range(
+                datetime.strptime(start, "%Y_%m_%d").date(),
+                datetime.strptime(end, "%Y_%m_%d").date(),
+                freq="D",
+            ):
+                sensor_files = list(
+                    sensor_path.glob(f"{sensor}_{dates.strftime('%Y_%m_%d')}*")
+                )
+                if len(sensor_files) != 1:
+                    continue
+
+                sensor_file: Path = sensor_files[0]
+                df_sensor = pd.read_parquet(sensor_file)
+                if "timestamp" in df_sensor.columns:
+                    df_sensor = df_sensor.set_index("timestamp")
+                df_list.append(df_sensor)
+            df_sensor = pd.concat(df_list)
+
+            for col in sorted(
+                set(df_sensor.columns.values).difference(["index", "timestamp"])
+            ):
+                print(f"{col}" + f" {len(df_sensor[col]):,} " + "-" * 30)
+                print(col, df_sensor[col].dtype)
+                fig.add_trace(
+                    trace=go.Scattergl(x=[], y=[], name=col),
+                    row=row_idx,
+                    col=1,
+                    hf_x=df_sensor[col].index,
+                    hf_y=df_sensor[col],
+                )
+            row_idx += 1
+    return fig
+
+
+# --------------------------------- Callbacks ---------------------------------
+# -------- plot or update graph ---------
+@app.callback(
+    Output("resampled-graph", "figure"),
+    ServersideOutput("store", "data"),
+    [
+        Input("plot-button", "n_clicks"),
+        State("checklist", "value"),
+        State("start-date", "value"),
+        State("end-date", "value"),
+        *selector_states,
+    ],
+)
+def plot_or_update_graph(_, selected_items, start_date, end_date, *folder_list):
+    selected_items = [] if selected_items is None else selected_items
+
+    it = iter(folder_list)
+    folder_user_day_sensor_list = []
+    for folder, user, subfolder, sensors in zip(it, it, it, it):
+        if not all((folder, user, subfolder, start_date, end_date)):
+            continue
+        else:
+            folder_user_day_sensor_list.append(
+                (folder, user, subfolder, start_date, end_date, sensors)
+            )
+
+    ctx = dash.callback_context
+    if (
+        len(ctx.triggered)
+        and "plot-button" in ctx.triggered[0]["prop_id"]
+        and len(folder_user_day_sensor_list)
+    ):
+        fig = plot_multi_sensors(
+            selected_items=selected_items,
+            fold_usr_subf_start_end_sensor_list=folder_user_day_sensor_list,
+        )
+        return fig, fig
+    return dash.no_update, dash.no_update
+
+
+if __name__ == "__main__":
+    app.run_server(port=8022, host="0.0.0.0", debug=True)
---
+++ code_utils/mbrain/visualization.py
@@ -0,0 +1,393 @@
+import os
+import textwrap
+from pathlib import Path
+from typing import List, Optional, Union
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from plotly_resampler import FigureResampler
+
+from code_utils.utils.visualizations import draw_line, draw_rectangle
+
+from .event_parsing import EventDumpParser
+from .headaches import construct_headache_day_df
+
+
+def construct_headache_event_hovertext(r: pd.Series) -> str:
+    td = r.endTime - r.time
+    hours = int(td.total_seconds() // 3600)
+    minutes = int((td.total_seconds() - 3600 * hours) // 60)
+
+    last_edited_time = None
+    if all(c in r for c in ["created_on", "updated_on"]):
+        last_edited_time = r[["created_on", "updated_on"]].dropna().max()
+
+    edit_time_str = ""
+    if not pd.isna(last_edited_time):
+        td_e = last_edited_time - r.endTime
+        hours_e = int(abs(td_e.total_seconds()) // 3600)
+        minutes_e = int((abs(td_e.total_seconds()) - 3600 * hours_e) // 60)
+        edit_time_str = (
+            "<br><b>tijdsverschil</b> t.o.v. einde: "
+            + ("-" if td_e.total_seconds() < 0 else "")
+            + f"{hours_e}:{minutes_e}"
+        )
+
+    created_on_str = (
+        (
+            f"<b>aangemaakt</b>: {r.created_on.strftime('%d/%m %H:%M')} "
+            if "created_on" in r and pd.notna(r.created_on)
+            else ""
+        )
+        + (
+            f"- laatst bewerkt: {last_edited_time.strftime('%d/%m %H:%M')}"
+            if "created_on" in r
+            and pd.notna(last_edited_time)
+            and last_edited_time > r.created_on
+            else ""
+        )
+        + edit_time_str
+    )
+    time_intensity_str = (
+        f"<b>Periode</b>: {r.time.strftime('%H:%M')} - "
+        f"{r.endTime.strftime('%H:%M')}" + f" - <b>duur</b>: {hours}h{minutes}min "
+        "- <b>intensiteit</b>: "
+        + str(r.intensity_str if "instensity_str" in r else r.intensity)
+    )
+    medication_str = (
+        f"<b>Nam medicatie</b>: {str(r.tookMedication)}  -  "
+        + f"<b>medicatie werkte</b>: {str(r.medicationWorked)}"
+    )
+
+    if isinstance(r.location, str):
+        loc_str = "<b>Locatie</b>:<br>   " + "<br>   ".join(
+            textwrap.wrap(r.location)
+        )  # + "<br>"
+    else:
+        loc_str = "<b>   no location</b>"
+
+    if isinstance(r.symptoms, str):
+        symp_str = "<b>Symptomen</b>:<br>   " + "<br>   ".join(
+            textwrap.wrap(r.symptoms, width=80)
+        )  # + "<br>"
+    else:
+        symp_str = "<b>   no symptoms<b>"
+
+    if isinstance(r.triggers, str):
+        trigger_str = "<b>Triggers</b>:<br>   " + "<br>   ".join(
+            textwrap.wrap(r.triggers)
+        )  # + "<br>"
+    else:
+        trigger_str = "<b>no triggers</b>"
+
+    return "<br>".join(
+        [
+            created_on_str,
+            time_intensity_str,
+            medication_str,
+            loc_str,
+            trigger_str,
+            symp_str,
+        ]
+    )
+
+
+def figs_to_html(
+    figs: List[go.Figure],
+    html_path: Union[Path, str],
+    append=False,
+    include_plotlyjs=True,
+):
+    """Save a list of figures in a single HTML file.
+
+    :param figs: A list of plotly figures
+    :param html_path: the HTML path where the figure will be saved
+    :param append:
+    """
+    if not isinstance(html_path, Path):
+        html_path = Path(html_path)
+
+    if not html_path.parent.exists():
+        os.makedirs(html_path.parent)
+
+    with open(html_path, "a" if append else "w") as f:
+        for fig in figs:
+            f.write(fig.to_html(full_html=False, include_plotlyjs=include_plotlyjs))
+
+
+def fast_scatter(df, subplots=False, mode="lines", **init_kwargs) -> FigureResampler:
+    if not subplots:
+        fig = FigureResampler(**init_kwargs)
+    else:
+        fig = FigureResampler(
+            make_subplots(rows=df.shape[1], cols=1, shared_xaxes=True)
+        )
+    for i, c in enumerate(df.columns, 1):
+        kwargs = {"row": i, "col": 1} if subplots else {}
+        fig.add_trace(
+            go.Scattergl(name=c, mode=mode), hf_x=df.index, hf_y=df[c], **kwargs
+        )
+    return fig
+
+
+def subplot_scatter(df, row_mode, **init_kwargs) -> go.Figure:
+    default_kwargs = {"shared_xaxes": True}
+    default_kwargs.update(init_kwargs)
+    fig = make_subplots(rows=len(row_mode), cols=1, **default_kwargs)
+    for i, (c_list, mode) in enumerate(row_mode, 1):
+        c_list = [c_list] if not isinstance(c_list, list) else c_list
+        for c in c_list:
+            fig.add_trace(
+                go.Scattergl(name=c, x=df.index, y=df[c], mode=mode), row=i, col=1
+            )
+    return fig
+
+
+def add_headache_timeline_to_fig(
+    fig: go.Figure,
+    row: int,
+    col: Optional[int] = 1,
+    df_eventdump: Optional[pd.DataFrame] = None,
+    df_medicine: Optional[pd.DataFrame] = None,
+    df_headache: Optional[pd.DataFrame] = None,
+    df_tag: Optional[pd.DataFrame] = None,
+    df_morning_headache: Optional[pd.DataFrame] = None,
+    show_headache_implicitness_row: Optional[bool] = True,
+) -> None:
+    if df_headache is None and df_medicine is None:
+        assert df_eventdump is not None
+
+    if df_eventdump is None:
+        df_eventdump = pd.DataFrame()
+    if df_headache is None:
+        df_headache = EventDumpParser.parse_headache_events(df_eventdump)
+        for c in ["time", "endTime"]:
+            df_headache[c] = df_headache[c].dt.tz_convert("Europe/Brussels")
+        df_headache = df_headache[df_headache.deprecated != True]
+    if df_medicine is None:
+        df_medicine = EventDumpParser.parse_medicine_events(df_eventdump)
+
+    # TODO -> clean up this code
+    date_range = pd.date_range(
+        df_headache.time.min().date(), df_headache.time.max().date(), freq="D"
+    )
+    date_range = date_range.tz_localize("Europe/Brussels")
+    print(date_range)
+    # add a shaded v_rect for the weekends
+    # make it clear to typehinding that the index is a datetime index
+    print(date_range[date_range.dayofweek == 5])
+    for sat in np.unique(date_range[date_range.dayofweek == 5].date):
+        fig.add_vrect(
+            x0=sat,
+            x1=sat + pd.Timedelta(days=2),
+            # fillcolor="rgba(0, 0, 0, .04)",
+            fillcolor="blue",
+            line_width=0,
+            row=1,
+        )
+
+    legend_names = []
+    # Add the data to the figure
+    for _, r in df_headache.iterrows():
+        fig.add_trace(
+            draw_rectangle(
+                t_start=r.time,
+                t_end=r.endTime,
+                row=1,
+                line=dict(color="rgba(255, 0, 0, 0.3)", width=1),  # red
+                mode="lines+markers",
+                marker_size=2,
+                name="headaches",
+                legendgroup="headaches",
+                showlegend="headaches" not in legend_names,
+                hovertext=construct_headache_event_hovertext(r),
+            ),
+            row=row,
+            col=col,
+        )
+        legend_names.append("headaches")
+
+    for _, r in df_medicine.iterrows():
+        fig.add_trace(
+            draw_line(
+                timestamp=r.time,
+                row=2,
+                marker_color="lightgreen",
+                opacity=0.7,
+                line_width=5,
+                name="medicine",
+                legendgroup="medicine",
+                hovertext=(
+                    f"<b>aangemaakt</b>: {r.created_on.strftime('%d/%m %H:%M')} "
+                    if "created_on" in r and pd.notna(r.created_on)
+                    else ""
+                )
+                + "<br>medicine:<br>    "
+                + r["medicine.name"]
+                + "<br>    "
+                + r["medicine.description"],
+                showlegend="medicine" not in legend_names,
+            ),
+            row=row,
+            col=col,
+        )
+        legend_names.append("medicine")
+
+    if df_tag is not None:
+        for ts, r in df_tag.iterrows():
+            # print(dict(r))
+            fig.add_trace(
+                draw_line(
+                    ts,
+                    row=2,
+                    line_color="black",
+                    name="tag",
+                    mode="lines",
+                    opacity=0.3,
+                    line_width=3,
+                    legendgroup="tag",
+                    text=f"processed by user: {dict(r).get('processed_by_user', 'NaN')}",
+                    showlegend="tag" not in legend_names,
+                ),
+                limit_to_view=False,
+                row=row,
+                col=col,
+            )
+            legend_names.append("tag")
+
+    if df_morning_headache is None:
+        return
+
+    color_list = px.colors.qualitative.Set2
+    for _, r in df_morning_headache.iterrows():
+        if r["answer.id"] == "yes":
+            if r.q_headaches_ref_date == 0:
+                # green - correct that there were no headaches
+                color = color_list[0]
+            else:
+                # red - correct that there were headaches
+                color = color_list[1]
+        else:
+            if r.q_headaches_ref_date == 0:
+                # red - incorrect that there were no headaches
+                color = px.colors.qualitative.Set1[0]
+            else:
+                # blue - unknown
+                color = color_list[2]
+        hovertext = (
+            f"gevraagd op {r.q_date.date()}<br>had u {r.q_headaches_ref_date} "
+            f"hoofdpijnaanvallen op {r.ref_date.date()}: <br>antwoord: {r['answer.value']}"
+        )
+
+        fig.add_trace(
+            draw_rectangle(
+                t_start=r.ref_date,
+                t_end=r.q_date,
+                row=3,
+                line=dict(color=color, width=1),
+                mode="lines+markers",
+                marker_size=2,
+                name="headache-implicitness",
+                legendgroup="headache-implicitness",
+                hovertext=hovertext,
+                showlegend="headache-implicitness" not in legend_names,
+            ),
+            row=row,
+            col=col,
+        )
+        legend_names.append("headache-implicitness")
+
+    # If both the morning questionnaire and the headache data are given: Perform a check
+    # to see if the headache data is consistent with the morning questionnaire
+    if (
+        not show_headache_implicitness_row
+        or df_morning_headache is None
+        or df_headache is None
+    ):
+        return
+
+    user_headache_day = construct_headache_day_df(df_headache)
+
+    for _, r in df_morning_headache.iterrows():
+        ref_series = user_headache_day[
+            user_headache_day.index.date == r.ref_date.date()
+        ].iloc[0]
+        any_headaches = (
+            ref_series.nbr_headaches_start_day > 0
+            or ref_series.nbr_headaches_end_day > 0
+            or ref_series.nbr_headaches_overlap_day > 0
+        )
+        any_no_headaches = (
+            ref_series.nbr_headaches_start_day == 0
+            or ref_series.nbr_headaches_end_day == 0
+            or ref_series.nbr_headaches_overlap_day == 0
+        )
+
+        if r["answer.id"] == "yes":
+            if r.q_headaches_ref_date == 0:
+                # green - correct that there were no headaches
+                color = color_list[0]
+
+                # IF there were headaches, then set the color to red
+                if not any_no_headaches:
+                    color = px.colors.qualitative.Set1[0]
+            else:
+                # RED color -> no perfect match between the morning questionnaire
+                # (indicating >0 headaches) and the headache data
+                color = px.colors.qualitative.Set1[0]
+                if (
+                    ref_series.nbr_headaches_start_day == r.q_headaches_ref_date
+                    or ref_series.nbr_headaches_end_day == r.q_headaches_ref_date
+                    or ref_series.nbr_headaches_overlap_day == r.q_headaches_ref_date
+                ):
+                    # green - exact match between the morning questionnaire
+                    color = color_list[0]
+        else:  # incorrect answer
+            if r.q_headaches_ref_date == 0:
+                # red - incorrect that there were no headaches
+                color = px.colors.qualitative.Set1[0]
+                if any_headaches:
+                    # orange - headache data is consistent with answer
+                    # (but we do not know how many)
+                    color = color_list[1]
+            else:
+                # red - incorrect
+                color = px.colors.qualitative.Set1[0]
+                if (
+                    ref_series.nbr_headaches_start_day != r.q_headaches_ref_date
+                    or ref_series.nbr_headaches_end_day != r.q_headaches_ref_date
+                    or ref_series.nbr_headaches_overlap_day != r.q_headaches_ref_date
+                ):
+                    # orange - headache data is consistent with answer
+                    # (but we do not know how many)
+                    color = color_list[1]
+        hovertext = (
+            f"gevraagd op {r.q_date.date()}<br>had u {r.q_headaches_ref_date} "
+            f"hoofdpijnaanvallen op {r.ref_date.date()}: "
+            f"<br>antwoord: {r['answer.value']}"
+            "<br>headaches which ... on this day:<br>"
+            f"   <b>start</b>: {ref_series.nbr_headaches_start_day} "
+            f" - <b>end</b>: {ref_series.nbr_headaches_end_day} "
+            f" - <b>overlap</b>: {ref_series.nbr_headaches_overlap_day},"
+        )
+
+        fig.add_trace(
+            draw_rectangle(
+                t_start=r.ref_date,
+                t_end=r.q_date,
+                row=4,
+                line=dict(color=color, width=1),
+                mode="lines+markers",
+                marker_size=2,
+                name="implicitness checks",
+                legendgroup="implicitness checks",
+                hovertext=hovertext,
+                showlegend="implicitness checks" not in legend_names,
+            ),
+            row=row,
+            col=col,
+        )
+        legend_names.append("implicitness checks")
